5d101bc20bbbdb51cde26db8560c6eda92ce4e13
ely211 <gonzalesm405@gmail.com>
2017-07-24 19:16:46 -0500

    HTTP verbs

--- server.js
+++ server.js
@@ -9,9 +9,33 @@ const port = process.env.PORT || 3001;
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.get('/clients/:name', (req, res)=>{
-    res.send({message: `Hola ${req.params.name}`});
-})
+app.get('/api/product', (request, response)=>{
+    response.send(200, {products: []});
+});
+
+app.get('/api/product/:productId', (request, response)=>{
+    let productId = request.params.productId;
+    if(error) return response.status(500).send({message: `Error al realizar la petición`});
+    response.status(200).send({productId});
+});
+
+app.post('/api/product', (request, response)=>{
+    console.log(request.body);
+    response.status(200).send({message: 'El producto se ha guardado'})
+});
+
+app.put('/api/product/:productId', (request, response)=>{
+    let productId = request.params.productId;
+    let update = request.body;
+    if(error) return response.status(500).send({message: `Error al actualizar producto: ${error}`});
+    response.status(200).send({message: `El producto ha sido actualizado`});    
+});
+
+app.delete('/api/product/:productId', (request, response)=>{
+    let productId = request.params.productId;
+    if(error) return response.status(500).send({message: `Error al borrar producto: ${error}`});
+    response.status(200).send({message: `El producto ha sido eliminado`});
+});
 
 app.listen(port, ()=>{
     console.log(`API RESTful en localhost:${port}`);
